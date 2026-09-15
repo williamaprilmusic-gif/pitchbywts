bdfa292279cac11edea25db513c8b6a3aa1730f2
williamaprilmusic-gif <williamaprilmusic@gmail.com>
2026-09-15 20:34:23 +0200

    Add live service status to Pitchline shell

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,13 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import InteractionEnhancer from './InteractionEnhancer';
+import LiveSystemStatus from './LiveSystemStatus';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <LiveSystemStatus />
     <InteractionEnhancer />
   </StrictMode>
 );
