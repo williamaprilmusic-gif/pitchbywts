f55cd20bf3c804bf3c83bcade7056ce6d4c163b1
williamaprilmusic-gif <williamaprilmusic@gmail.com>
2026-09-15 20:53:18 +0200

    Mount professional operations suite across Pitchline

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import InteractionEnhancer from './InteractionEnhancer';
 import LiveSystemStatus from './LiveSystemStatus';
+import ProfessionalSuite from './ProfessionalSuite';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
@@ -10,5 +11,6 @@ createRoot(document.getElementById('root')!).render(
     <App />
     <LiveSystemStatus />
     <InteractionEnhancer />
+    <ProfessionalSuite />
   </StrictMode>
 );
