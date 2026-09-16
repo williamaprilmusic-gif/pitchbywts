56c78bab64c7e5b7a909346b49cc36f5a6750e17
williamaprilmusic-gif <williamaprilmusic@gmail.com>
2026-09-16 08:15:38 +0200

    Expose authenticated QA cleanup endpoint

--- api/index.js
+++ api/index.js
@@ -1,3 +1,10 @@
 import api from '../server/apiRuntime.mjs';
+import { cleanupProductionQa } from '../server/qaCleanup.js';
 
-export default api;
+export default async function handler(request, response) {
+  const path = String(request.query?.path || '').replace(/^\/+|\/+$/g, '');
+  if (request.method === 'POST' && path === 'admin/qa-cleanup') {
+    return cleanupProductionQa(request, response);
+  }
+  return api(request, response);
+}
